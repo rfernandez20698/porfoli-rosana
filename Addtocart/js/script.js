f69d0faf0de7fcc9833c065e4532a7d2be2dad48
rfernandez20698 <rfernandez20698@iesjoanramis.org>
2024-10-16 11:53:19 +0200

    Add files via upload

---
+++ Addtocart/js/script.js
@@ -0,0 +1,8 @@
+const input=document.getElementById("imputField");
+const boto= document .getElementById("afegir");
+
+boto.addEventListener("click", function(){
+
+    console.log(input.value)
+
+})
