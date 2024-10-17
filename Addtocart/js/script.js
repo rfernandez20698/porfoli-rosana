977d7e54a854897bd21a2dca3c6d1d68cf52b43b
rfernandez20698 <rfernandez20698@iesjoanramis.org>
2024-10-17 08:56:45 +0200

    Add files via upload

--- Addtocart/js/script.js
+++ Addtocart/js/script.js
@@ -1,7 +1,22 @@
-const input=document.getElementById("imputField");
-const boto= document .getElementById("afegir");
+import{ initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-boto.addEventListener("click", function(){
+
+
+const input = document.getElementById("inputField");
+const boto = document.getElementById("afegir");
+
+const appSettings = {
+    databaseURL: "https://addtocart-367b1-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp (appSettings);
+const baseDades = getDatabase(app);
+const tasks = ref(baseDades, "tareas");
+
+boto.addEventListener("click", function () {
+    push(tasks, input.value)
+    alert ("Afegir a la BD");
 
     console.log(input.value)
 
